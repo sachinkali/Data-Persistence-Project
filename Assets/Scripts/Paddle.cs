125c8d7a0ad18d18df2377d9896adf25f4f5660a
sachinkali <140141198+sachinkali@users.noreply.github.com>
2023-11-06 11:54:56 -0600

    float speed increases by 1

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    public float Speed = 2.0f;
+    public float Speed = 3.0f;
     public float MaxMovement = 2.0f;
     
     // Start is called before the first frame update
